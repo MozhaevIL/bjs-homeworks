945bb8a6e24aab4d77c8badd2893775482b176dc
MozhaevIL <ptitzzz@gmail.com>
2020-03-24 19:38:17 +0300

    Fixed task 2

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -18,7 +18,7 @@ function sayHello(userName) {
 	
 	let myName = userName;
 	    
-	return 'Привет, мир! Меня зовут ${myName}';
+	return `Привет, мир! Меня зовут ${myName}`;
 }
 
 function calculateFormula() {
