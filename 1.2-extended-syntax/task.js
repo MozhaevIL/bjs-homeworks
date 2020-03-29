f27d4f09c399aede5479904cd9bba3f906f776e1
MozhaevIL <ptitzzz@gmail.com>
2020-03-29 15:43:24 +0300

    task 1 fixed 2.5

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -3,7 +3,7 @@ function getResult(a,b,c) {
   let d, x = [];
   d = b ** 2 - 4 * a * c;
   if (d === 0) {
-   x = x.push(- b / 2 * a);
+   x[0] = - b / 2 * a;
      }
   else if (d > 0) {
     x[0] = (-b - d ** 0.5) / 2 * a;
