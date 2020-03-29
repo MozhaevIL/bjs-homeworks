5bd9d9236c0bd3bebf8b99a12c58b663885e5747
MozhaevIL <ptitzzz@gmail.com>
2020-03-29 19:13:56 +0300

    task 1 fixed 1.1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -3,25 +3,25 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
 
  let month;
 
-  if (isNaN(percent) = true) {
+  if (isNaN(percent) === true) {
      alert(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
      } else {
      percent = Number(percent);
     }
 
-  if (isNaN(contribution) = true) {
+  if (isNaN(contribution) === true) {
      alert(`Параметр "начальный взнос" содержит неправильное значение ${contribution}`);
      } else {
      contribution = Number(contribution);
     }
    
-   if (isNaN(amount) = true) {
+   if (isNaN(amount) === true) {
      alert(`Параметр "общая стоимость" содержит неправильное значение ${amount}`);
      } else {
      amount = Number(amount);
     }
 
-    if (isNaN(date) = true) {
+    if (isNaN(date) === true) {
      alert(`Параметр "срок ипотеки" содержит неправильное значение ${date}`);
      } else {
      months = Math.round((dateTarget.getTime() - dateNow.getTime()) / (1000 * 3600 * 24 * 30));
