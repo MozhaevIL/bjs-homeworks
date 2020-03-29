9821aaabb1125c37c19a70ee8223cdc69703a35a
MozhaevIL <ptitzzz@gmail.com>
2020-03-29 22:06:09 +0300

    task 1 fixed 1.8

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,7 +1,7 @@
 "use strict"
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
- let months = Math.round((date.getTime() - new Date().getTime()) / (1000 * 3600 * 24 * 30));
+ let months = Math.floor((date.getTime() - new Date().getTime()) / (1000 * 3600 * 24 * 30));
 
   if (isNaN(percent) === true) {
      alert(`Параметр "процентная ставка" содержит неправильное значение ${percent}`);
