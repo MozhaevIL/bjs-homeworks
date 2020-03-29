b43db34bc5cc04bdfa6c1de081beeef85d4f0b68
MozhaevIL <ptitzzz@gmail.com>
2020-03-29 15:06:48 +0300

    task 1 completed

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,16 @@
+"use strict"
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+  let d, x = [];
+  d = b ** 2 - 4 * a * c;
+  if (d = 0) {
+   x[0] = - b / 2 * a;
+     }
+  else if (d>0) {
+    x[0] = (-b - d ** 0.5) / 2 * a;
+    x[1] = (-b + d ** 0.5) / 2 * a;
+   }   
+  }
+return x;
 }
 
 function getAverageMark(marks){
