5964861d4956934321a3d9caee19076ebaaea3f1
MozhaevIL <ptitzzz@gmail.com>
2020-04-05 12:01:07 +0300

    Выполнена задача 1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,6 +1,10 @@
 function getAnimalSound(animal) {
-    // код для задачи №1 писать здесь
-    // return animalSound;
+    if(Object.keys(animal).length === 0) {
+        return null;
+    } else {
+        let sound = this.sound;
+        return sound;
+    }
 }
 
 function getAverageMark(marks) {
