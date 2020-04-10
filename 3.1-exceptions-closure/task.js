66791b62f7708266b2df2805e6f60760256fd94b
MozhaevIL <ptitzzz@gmail.com>
2020-04-10 15:56:49 +0300

    Добавлено предварительное  решение задачи 2

--- 3.1-exceptions-closure/task.js
+++ 3.1-exceptions-closure/task.js
@@ -15,3 +15,35 @@ function validateCount(inputNumber) {
     }
     return parseCount(inputNumber);
 }
+
+//Задача 2
+class Triangle {
+    constructor(a,b,c) {
+        if((a >= b + c) || (b >= a + c) ||(c >= a + b)) {
+          throw new Error("Треугольник с такими сторонами не существует");
+        }
+        
+        this.a = a;
+        this.b = b;
+        this.c = c;
+      }
+    
+
+    getPerimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    getArea() {
+        let p = this.getPerimeter() / 2;
+        let s = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return s.toFixed(3);
+    }
+}
+
+function getTriangle(a,b,c) {
+    try {
+        return new Triangle(a,b,c);
+    } catch (error) {
+        console.log("Ошибка! Неправильный треугольник");
+    }
+}
